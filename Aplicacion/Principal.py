840327c2951194c1f21a7ae1520fa4cc9359b106
Marcos Barranquero <marcosbarranquero@outlook.es>
2020-09-19 17:47:38 +0200

    Add files via upload

--- Aplicacion/Principal.py
+++ Aplicacion/Principal.py
@@ -5,17 +5,23 @@ from Aplicacion.BotTelegram.BorrarCuentaHandler import BorrarCuentaHandler
 from Aplicacion.BotTelegram.RegistroHandler import RegistroHandler
 from Aplicacion.BotTelegram.WallascraperBot import Wallascraper
 
-TOKEN = "INSERTA TU TOKEN AQUÍ"
+TOKEN = "1365812866:AAECLY6QKTMBbIkudhvdvyRujbjYj9s4HNE"
 
 if __name__ == "__main__":
-    gestorBBDD = GestorBBDD()
+    # Creo handlers de conversaciones
     registroHandler = RegistroHandler()
     borrarCuentaHandler = BorrarCuentaHandler()
     addBusquedaHandler = AddBusquedaHandler()
     borrarBusquedaHandler = BorrarBusquedaHandler()
+
+    # Creo bot
     bot = Wallascraper(TOKEN)
+
+    # Añado handlers
     bot.addHandler(registroHandler.handler)
     bot.addHandler(addBusquedaHandler.handler)
     bot.addHandler(borrarBusquedaHandler.handler)
     bot.addHandler(borrarCuentaHandler.handler)
+
+    # Lo pongo a correr en IDLE 
     bot.run()
