4e2249d6c49fce03732434df5efb55f4b44279d7
Marcos Barranquero <marcosbarranquero@outlook.es>
2020-09-26 15:42:30 +0200

    Notificación de finalización de scrapeo
    Añadida notificación de finalización de scrapeo para telegram.

--- Aplicacion/BotTelegram/Notificador.py
+++ Aplicacion/BotTelegram/Notificador.py
@@ -1,6 +1,8 @@
 #from Aplicacion.BotTelegram.WallascraperBot import TOKEN
 
 import telegram
+from datetime import datetime
+
 
 class Notificador:
     def __init__(self, TOKEN):
@@ -13,3 +15,8 @@ class Notificador:
     def notificarAnuncio(self, id_usuario, articulo, busqueda):
         texto = f"¡Nuevo {articulo.titulo} por {articulo.precio}!, de tu búsqueda {busqueda.keywords}. Échale un vistazo: {articulo.enlace}"
         self.bot.sendMessage(id_usuario, text=texto)#, reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    def notificarFinScrapeo(self, id_usuario):
+        hoy = datetime.now()
+        texto = f"[{hoy.day}/{hoy.month}/{hoy.year} - {hoy.hour}:{hoy.minute}] Finalizado scapeo. "
+        self.bot.sendMessage(id_usuario, text=texto)
